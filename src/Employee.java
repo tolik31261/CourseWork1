4089ef43f8717d20bd05d90c3be677c80d618748
User <nazarovtolya2@gmail.com>
2023-03-19 23:15:44 +0500

    Задания 1-6

--- src/Employee.java
+++ src/Employee.java
@@ -1,12 +1,19 @@
 public class Employee {
+    private static long idCounter = 0;
+    private long id;
     private String fullName;
     private int department;
     private double salary;
+
+    private Employee() {
+        this.id = idCounter++;
+    }
 
     public Employee(String fullName, int department, double salary) {
         this.fullName = fullName;
         this.department = department;
         this.salary = salary;
+        this.id = idCounter++;
     }
 
     public String getFullName() {
