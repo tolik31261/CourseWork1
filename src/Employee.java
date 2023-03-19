ec909b3d0634cefead85bb397e8d5e9316578648
User <nazarovtolya2@gmail.com>
2023-03-19 23:05:44 +0500

    Конструкторы, геттеры и сеттеры

---
+++ src/Employee.java
@@ -0,0 +1,30 @@
+public class Employee {
+    private String fullName;
+    private int department;
+    private double salary;
+
+    public Employee(String fullName, int department, double salary) {
+        this.fullName = fullName;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
